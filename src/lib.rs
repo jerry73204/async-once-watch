d43b3537d174c1d8fe5f173e1107e06f6e2bd9fc
aeon <jerry73204@gmail>
2022-02-20 17:12:07 +0800

    Write doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,44 @@
+//! Asynchronous and shareable container which value is set once.
+//!
+//! The [OnceWatch<T>](OnceWatch) is a container with [`get()`](OnceWatch::get) and [`set()`](OnceWatch::set) methods.
+//! The values is set at most once. The readers call `get()` and wait until the value is ready.
+//!
+//! ```rust
+//! use async_once_watch::OnceWatch;
+//! use async_std::task::{sleep, spawn};
+//! use once_cell::sync::Lazy;
+//! use std::time::Duration;
+//!
+//! static STATE: Lazy<OnceWatch<u8>> = Lazy::new(OnceWatch::new);
+//! let secret = 10;
+//!
+//! /* Writer */
+//! spawn(async move {
+//!     sleep(Duration::from_millis(500)).await;
+//!
+//!     // First write is fine
+//!     let ok = STATE.set(secret).is_ok();
+//!     assert!(ok);
+//!
+//!     // Second write is not allowed
+//!     let ok = STATE.set(secret).is_ok();
+//!     assert!(!ok);
+//! });
+//!
+//! /* Reader */
+//! spawn(async move {
+//!     let received = *STATE.get().await;
+//!     assert_eq!(received, secret);
+//! });
+//! ```
+
 use event_listener::Event;
 use std::{
     ptr,
     sync::atomic::{AtomicPtr, Ordering::*},
 };
 
+/// The shareable container which value is set once.
 #[derive(Debug)]
 pub struct OnceWatch<T>
 where
@@ -17,6 +52,7 @@ impl<T> OnceWatch<T>
 where
     T: Sync,
 {
+    /// Creates a new uninitialized instance.
     pub fn new() -> Self {
         let event = Event::new();
         Self {
@@ -25,6 +61,9 @@ where
         }
     }
 
+    /// Sets the value to the container.
+    ///
+    /// It returns `Ok` in first call and `Err` in later calls.
     pub fn set(&self, data: T) -> Result<(), T> {
         let data = Box::new(data);
         let ptr = Box::into_raw(data);
@@ -45,6 +84,7 @@ where
         }
     }
 
+    /// Waits until the value is set and obtains the reference.
     pub async fn get(&self) -> &T {
         let listener = self.event.listen();
         listener.await;
@@ -52,6 +92,9 @@ where
         unsafe { ptr.as_ref().unwrap() }
     }
 
+    /// Try to get the value reference in non-blocking manner.
+    ///
+    /// It returns `None` if the value is not ready.
     pub fn try_get(&self) -> Option<&T> {
         let ptr = self.data.load(Acquire);
         unsafe { ptr.as_ref() }
